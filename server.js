09476a064e9f84b4452fa3f8b0f050f96a997ef5
shyamjoshi123 <shyamjoshi.g123@gmail.com>
2023-02-04 15:00:06 +1100

    final commit
    final commit

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-var express = require("express");
-const res = require("express/lib/response");
+var express = require("express")
 var app = express()
-
-
+var port = process.env.port || 3000;
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
-app.use(express.urlencoded({extends: false}));
+app.use(express.urlencoded({extends:false}))
 
-const addNumbers= (number1, number2) => {
+
+//get api for add
+app.get("/addTwoNumbers", (req,res) => {
+   var number1 = req.query.number1;
+   var number2 = req.query.number2;
+   var result = addNumbers(number1, number2)
+   res.json({statusCode: 200, data: result, message:'Success'})
+})
+const addNumbers = (number1, number2) => {   
     var num1 = parseInt(number1)
     var num2 = parseInt(number2)
-    var result = num1 + num2;
+    var result = num1+num2;
     return result;
-}
-
-app.get("/addTwoNumbers",(req,res) => {
-    var number1 = req.query.number1;
-    var number2 = req.query.number2;
-    var result = addNumbers(number1, number2) 
-    res.json({statusCode: 100, data: result, message: "success"})
-
+}    
+//subtract
+app.get("/subtractTwoNumbers", (req,res) => {
+   var number1 = req.query.number1;
+   var number2 = req.query.number2;
+   var result = subtractNumbers(number1, number2)
+   res.json({statusCode: 200, data: result, message:'Success'})
 })
-var port = process.env.port || 3000;
-
+const subtractNumbers = (number1, number2) => {   
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1-num2;
+    return result;
+}    
+var port= process.env.port||3000;
 app.listen(port,()=>{
-    console.log("App listening to: http://localhost: "+port)
-}
-)
+    console.log("App listening to https://localhost: "+port)
+ })
